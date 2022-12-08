b8203f4cf0eb6a3c5fc3963173f076c3a154d8da
bjreddy009037 <bjreddy246@gmail.com>
2022-12-08 13:57:54 +0000

    jenkins

--- roboshop/common/ec2.tf
+++ roboshop/common/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terra-ec2" {
   ami           = var.AMI
   instance_type = var.INSTANCE_TYPE
-  vpc_security_group_ids = aws_security_group.allow_all.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.COMPONENT
