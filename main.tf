b89b380680f60c8365c22bbd8a71c8e1715eca0f
bjreddy009037 <bjreddy246@gmail.com>
2022-12-08 10:39:06 +0000

    jenkins

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "= 4.42.0" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+}
+
