5c6278dd2612f59dcd0e2dbecfd263b8e63eb42b
bjreddy009037 <bjreddy246@gmail.com>
2022-12-08 10:49:29 +0000

    jenkins

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "terra-ec2" {
   ami           = "ami-0bb6af715826253bf"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = "testing"
