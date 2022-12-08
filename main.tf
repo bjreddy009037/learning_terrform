c3d9b6a13c71f0722fa0bf30f54258162d1e78bd
bjreddy009037 <bjreddy246@gmail.com>
2022-12-08 11:01:38 +0000

    jenkins

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 # Terraform Settings Block
 terraform {
+  backend "s3" {
+    bucket = "terraformforme"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
